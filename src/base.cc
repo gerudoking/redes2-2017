bd62280e76a17e0d174bb3c902158e1c4afa901d
gerudoking <edsantos2003@gmail.com>
2017-11-27 21:44:46 -0200

    Base setada para funcionar como cliente servidor UDP. P2P está comentado.

--- src/base.cc
+++ src/base.cc
@@ -1,5 +1,7 @@
+#include <fstream>
 #include "ns3/netanim-module.h"
 #include "ns3/core-module.h"
+#include "ns3/csma-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
@@ -15,51 +17,88 @@ int main (int argc, char *argv[]){
 	LogComponentEnable("UdpEchoClientApplication",LOG_LEVEL_INFO);
 	LogComponentEnable("UdpEchoServerApplication",LOG_LEVEL_INFO);
 
-	NodeContainer nodes;
-        nodes.Create(11);
+	Address serverAddress;
+	bool useV6 = false;
+
+	//Cria os nós da rede
+	NS_LOG_INFO("Creating nodes...");
+	NodeContainer nodes, nodesTotal, nodesRede1, nodesRede2;
+	nodes.Create(11);
+
+	//Seta os nós para cada rede
+
+	//Rede 1
+	nodesRede1.Add(nodes.Get(0));
+	nodesRede1.Add(nodes.Get(1));
+	nodesRede1.Add(nodes.Get(2));
+	nodesRede1.Add(nodes.Get(3));
+	nodesRede1.Add(nodes.Get(4));//Nó roteador da rede 1
+
+	//Rede 2
+	nodesRede2.Add(nodes.Get(7));
+	nodesRede2.Add(nodes.Get(8));
+	nodesRede2.Add(nodes.Get(9));
+	nodesRede2.Add(nodes.Get(10));
+	nodesRede2.Add(nodes.Get(6));//Nó roteador da rede 2
+
+	//Rede entre roteadores e nó intermediário
+	nodesTotal.Add(nodes.Get(4));
+	nodesTotal.Add(nodes.Get(5));
+	nodesTotal.Add(nodes.Get(6));
 
 	InternetStackHelper stack;
 	stack.Install(nodes);
 
+	//Cria os canais requeridos pela topologia
+	NS_LOG_INFO("Creating channels...");
+
+	CsmaHelper csma;
+	csma.SetChannelAttribute("DataRate", DataRateValue(DataRate (5000000)));
+	csma.SetChannelAttribute("Delay", TimeValue(MilliSeconds(2)));
+	csma.SetDeviceAttribute("Mtu", UintegerValue(1400));
+
+	NetDeviceContainer d = csma.Install(nodes);
+
+	/*
 	PointToPointHelper p2p1;
 	p2p1.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
 	p2p1.SetChannelAttribute ("Delay", StringValue ("1ms"));
 
-        PointToPointHelper p2p2;
-        p2p2.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p2.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p2;
+    p2p2.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p2.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p3;
-        p2p3.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p3.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p3;
+    p2p3.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p3.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p4;
-        p2p4.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p4.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p4;
+    p2p4.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p4.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p5;
-        p2p5.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p5.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p5;
+    p2p5.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p5.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p6;
-        p2p6.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p6.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p6;
+    p2p6.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p6.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p7;
-        p2p7.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p7.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p7;
+    p2p7.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p7.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p8;
-        p2p8.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p8.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p8;
+    p2p8.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p8.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p9;
-        p2p9.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p9.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p9;
+    p2p9.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p9.SetChannelAttribute("Delay", StringValue("5ms"));
 
-        PointToPointHelper p2p10;
-        p2p10.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
-        p2p10.SetChannelAttribute("Delay", StringValue("5ms"));
+    PointToPointHelper p2p10;
+    p2p10.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    p2p10.SetChannelAttribute("Delay", StringValue("5ms"));
 
 	Ipv4AddressHelper address;
 	address.SetBase("10.1.1.0", "255.255.255.0");
@@ -102,8 +141,42 @@ int main (int argc, char *argv[]){
 	devices = p2p10.Install(nodes.Get(6),nodes.Get(10));
 	address.SetBase("10.1.10.0","255.255.255.0");
 	interfaces = address.Assign(devices);
+	*/
 
-	Ipv4GlobalRoutingHelper::PopulateRoutingTables();	
+	NS_LOG_INFO ("Assign IP Addresses.");
+  	if (useV6 == false){
+      	Ipv4AddressHelper ipv4;
+      	ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+      	Ipv4InterfaceContainer i = ipv4.Assign (d);
+      	serverAddress = Address (i.GetAddress (1));
+    }
+  	else{
+      	Ipv6AddressHelper ipv6;
+      	ipv6.SetBase ("2001:0000:f00d:cafe::", Ipv6Prefix (64));
+     	Ipv6InterfaceContainer i6 = ipv6.Assign (d);
+      	serverAddress = Address(i6.GetAddress (1,1));
+    }
+
+    NS_LOG_INFO("Create Applications.");
+
+	uint16_t port = 4000;
+  	UdpServerHelper server (port);
+  	ApplicationContainer apps = server.Install(nodes.Get (0));
+  	apps.Start (Seconds (1.0));
+  	apps.Stop (Seconds (10.0));
+
+  	uint32_t MaxPacketSize = 1024;
+  	Time interPacketInterval = Seconds (0.05);
+  	uint32_t maxPacketCount = 320;
+  	UdpClientHelper client (serverAddress, port);
+  	client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+  	client.SetAttribute ("Interval", TimeValue (interPacketInterval));
+  	client.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
+  	apps = client.Install (nodes.Get (0));
+  	apps.Start (Seconds (2.0));
+  	apps.Stop (Seconds (10.0));
+
+	/*Ipv4GlobalRoutingHelper::PopulateRoutingTables();	
 
 	UdpEchoServerHelper echoServer(9);
 	
@@ -119,19 +192,20 @@ int main (int argc, char *argv[]){
 	ApplicationContainer clientApps = echoClient.Install (nodes.Get(0));
 	clientApps.Start(Seconds(2.0));
 	clientApps.Stop(Seconds(10.0));
+	*/
 
 	AnimationInterface anima ("base.xml");
 	anima.SetConstantPosition (nodes.Get(0),0.0,0.0);
-	anima.SetConstantPosition (nodes.Get(1),2.0,2.0);
-	anima.SetConstantPosition (nodes.Get(2),3.0,3.0);
-	anima.SetConstantPosition (nodes.Get(3),4.0,4.0);
-	anima.SetConstantPosition (nodes.Get(4),5.0,5.0);
-	anima.SetConstantPosition (nodes.Get(5),6.0,6.0);
-	anima.SetConstantPosition (nodes.Get(6),7.0,7.0);
-	anima.SetConstantPosition (nodes.Get(7),8.0,8.0);
-	anima.SetConstantPosition (nodes.Get(8),9.0,9.0);
-	anima.SetConstantPosition (nodes.Get(9),10.0,10.0);
-	anima.SetConstantPosition (nodes.Get(10),11.0,11.0);
+	anima.SetConstantPosition (nodes.Get(1),0.0,20.0);
+	anima.SetConstantPosition (nodes.Get(2),0.0,40.0);
+	anima.SetConstantPosition (nodes.Get(3),0.0,60.0);
+	anima.SetConstantPosition (nodes.Get(4),30.0,30.0);
+	anima.SetConstantPosition (nodes.Get(5),60.0,0.0);
+	anima.SetConstantPosition (nodes.Get(6),90.0,30.0);
+	anima.SetConstantPosition (nodes.Get(7),120.0,0.0);
+	anima.SetConstantPosition (nodes.Get(8),120.0,20.0);
+	anima.SetConstantPosition (nodes.Get(9),120.0,40.0);
+	anima.SetConstantPosition (nodes.Get(10),120.0,60.0);
 
 	Simulator::Run();
 	Simulator::Destroy();
